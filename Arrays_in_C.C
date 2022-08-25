f26d8a3de825b95721cd352d697ffeeaec879ee0
harsh007-github <khandelwalharsh401@gmail.com>
2022-08-25 18:49:57 +0530

    Arrays_in_C Updated

--- Arrays_in_C.C
+++ Arrays_in_C.C
@@ -110,3 +110,28 @@ int main(){
 
 	return 0;
 }
+
+	//Arrays as Function Argument
+
+	int sumofarray(int A[],int size)
+{
+	int i;
+	int sum=0;
+	for(i=0;i<size;i++)
+	{
+	sum+=A[i];
+	}
+	return sum;
+}
+
+int main()
+	{	
+	int A[]={1,2,3,4,5};
+	int size=sizeof (A)/sizeof (A[0]);
+	int SumOF=sumofarray(A,size);
+	printf("Sum Of Elements:%d\n",SumOF);
+	printf("%d",sizeof A);
+	return 0;
+
+}
+
