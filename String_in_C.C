6fcafbe24f27747da040aedb9655bc6f7c6ef870
harsh007-github <khandelwalharsh401@gmail.com>
2022-09-10 18:20:28 +0530

    Content added

--- String_in_C.C
+++ String_in_C.C
@@ -1,8 +1,20 @@
 #include<stdio.h>
 int main ()
 {
-
+	int str[5]="Harsh";
+	printf("%s",str);	
 
 
 return 0;
 }
+//Transferring elements from one string to another
+char s[]="Harsh";
+	char t[2];
+	int i;
+	for(i=0;s[i]!='\0';i++)
+	{
+	t[i]=s[i];
+	}
+	t[i]='\0';
+	printf("%s",t);
+	return 0;
