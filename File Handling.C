bc7f202ede697b0a63dd772a75c188c04332e609
harsh007-github <khandelwalharsh401@gmail.com>
2022-10-07 21:31:37 +0530

    fputc() added

--- File Handling.C
+++ File Handling.C
@@ -1,4 +1,5 @@
-//Creating a .txt file using fopen() using reading("r") and writing("w") attributes.
+//Creating a .txt file using fopen() using reading("r") and writing("w") attributes
+
 #include<stdio.h>
 #include<conio.h>
 int main()
@@ -11,6 +12,33 @@ int main()
 	}
 	else
 	{
+	printf("File opened Successfully");
+	}
+	return 0;
+}
+
+//Entering text in a file using putc()
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	FILE *Fpointer;	//Pointer to the file
+	Fpointer=fopen("Harsh.txt","a");
+	char data[100]="Hello there fellow programmer";
+	int i;
+	int length=strlen(data);
+	if(Fpointer==NULL)
+	{
+	printf("Unable to open file");
+	}
+	else
+	{
+	for(i=0;i<length;i++)
+	{
+	 fputc(data[i],Fpointer);
+	}
 	printf("File opened Successfully");
 	}
 	return 0;
