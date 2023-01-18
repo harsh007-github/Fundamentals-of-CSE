f3deb1c8e441899e62dbbcc7de2beb8f60bf27fc
harsh007-github <khandelwalharsh401@gmail.com>
2023-01-18 16:07:45 +0530

    User sign in panel program added in JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,31 @@
-var num = 25;        //Declaring a variable of type var named "num" with value 1
-console.log(name);  //Printng num
+//Most basic JS program
+var numb = 25;        //Declaring a variable of type var named "num" with value 1
+console.log("Variable value is " + numb);  //Printng num
+
+//Creating a JS program for user sign in portal of a website(Basic)
+
+const Uid = HAR123; //Contain UniqueID of the user
+
+var firstName = "Harsh";        //User Information
+var lastName = "Khandelwal";
+var email = "harsh18@gmail.com";
+var password = "123456";
+var country = "India";
+var state = "Rajasthan";
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+
+console.log(`
+    User with UID   : ${Uid}
+    First Name      : ${firstName}
+    Last Name       : ${lastName}  
+    Email ID        : ${email}
+    password        : ${password}
+    Country         : ${country}
+    State           : ${state}
+    Google Log-In   : ${isLoggedInFromGoogle}
+    Facebook Log-In : ${isLoggedInFromFacebook}
+
+`);
+
+
