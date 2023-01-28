38254f355f04e50a5839b600788b4e1b93c14919
harsh007-github <khandelwalharsh401@gmail.com>
2023-01-28 11:25:05 +0530

    Operator Overloading added

--- OOPs in C++/OOPS.cpp
+++ OOPs in C++/OOPS.cpp
@@ -166,3 +166,91 @@ int main()
 	myClass m(15);
 	return 0;
 }
+
+//Program to illustrate UnaryOperator Overloading
+#include<iostream>
+using namespace std;
+class complex
+{
+	private:
+		int a , b;
+	public:
+		void setData(int x , int y)
+		{
+			a = x , b = y;
+		}
+		void showData()
+		{
+			cout << "a = " << a << " b = " << b;
+		}
+		complex operator -()
+		{
+			complex temp;
+			temp.a = -(a);
+			temp.b = -(b);
+			return temp;
+		}
+};
+
+int main()
+{
+	complex c1 , c2;
+	c1.setData(15 , 4);
+	c2 = -c1;
+	c2.showData();
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    private:
+      float real;
+      float imag;
+    public:
+       Complex(): real(0), imag(0){ }
+       void input()
+       {
+           cout << "Enter real and imaginary parts respectively: ";
+           cin >> real;
+           cin >> imag;
+       }
+
+       // Operator overloading
+       Complex operator - (Complex c2)
+       {
+           Complex temp;
+           temp.real = real - c2.real;
+           temp.imag = imag - c2.imag;
+
+           return temp;
+       }
+
+       void output()
+       {
+           if(imag < 0)
+               cout << "Output Complex number: "<< real << imag << "i";
+           else
+               cout << "Output Complex number: " << real << "+" << imag << "i";
+       }
+};
+
+int main()
+{
+    Complex c1, c2, result;
+
+    cout<<"Enter first complex number:\n";
+    c1.input();
+
+    cout<<"Enter second complex number:\n";
+    c2.input();
+
+    // In case of operator overloading of binary operators in C++ programming,
+    // the object on right hand side of operator is always assumed as argument by compiler.
+    result = c1 - c2;
+    result.output();
+
+    return 0;
+}
