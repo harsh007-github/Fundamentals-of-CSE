1ace0a49ed2562cec1492a3073ab825657837dcc
harsh007-github <khandelwalharsh401@gmail.com>
2023-02-12 20:37:10 +0530

    Multi Level inheritance added

--- OOPs in C++/Inheritance/Inheritance.cpp
+++ OOPs in C++/Inheritance/Inheritance.cpp
@@ -1,4 +1,4 @@
-//Program to illustrate Single Inheritance 
+//Program to illustrate Single Inheritance
 #include<iostream>
 using namespace std;
 class car
@@ -33,5 +33,57 @@ int main()
 	s.setName("Supra");
 	s.setSpeed(36);
 	s.showData();
+	return 0;
+}
+
+// Program to illustrate Multilevel Inheritance
+#include<iostream>
+using namespace std;
+class length
+{
+	protected:
+		float len;
+	public:
+		void setLength(float l)
+		{
+			len = l;
+		}
+};
+class width : public length
+{
+	protected:
+		float wid;
+	public:
+		void setWidth(float w)
+		{
+			wid = w;
+		}
+};
+class height : public width
+{
+	protected:
+		float hgt;
+	public:
+		void setHeight(float h)
+		{
+			hgt = h;
+		}
+
+};
+class volume : public height
+{
+	public:
+		void printVolume()
+		{
+			cout << "Volume of the cuboid is " << len * wid * hgt;
+		}
+};
+int main()
+{
+	volume v;
+	v.setHeight(5);
+	v.setLength(6);
+	v.setWidth(7);
+	v.printVolume();
 	return 0;
 }
