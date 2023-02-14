09bcd253223eedb0f32fb2dbadfbd8961b1b7664
harsh007-github <khandelwalharsh401@gmail.com>
2023-02-14 21:47:39 +0530

    Hierarchical inheritance added

--- OOPs in C++/Inheritance/Inheritance.cpp
+++ OOPs in C++/Inheritance/Inheritance.cpp
@@ -130,3 +130,46 @@ int main()
 	a.printArea();
 	return 0;
 }
+
+// Program to illustrate Hierarchical Inheritance
+#include<iostream>
+using namespace std;
+class data
+{
+	protected:
+		float rad, hgt;
+	public:
+		void setData(float r , float h)
+		{
+			rad = r;
+			hgt = h;
+		}
+
+};
+class surfArea : public data
+{
+	public:
+	void surfaceArea()
+	{
+		cout << "Surface area of cylinder is " <<  2 * 3.14 * rad *  hgt << endl;
+	}
+};
+class volume : public data
+{
+	public:
+		void volumeCylndr()
+		{
+			cout << "Volume of cylinder is " << 3.14 * rad * rad * hgt << endl;
+		}
+};
+
+int main()
+{
+	volume v;
+	v.setData(15 , 30);
+	v.volumeCylndr();
+	surfArea s;
+	s.setData(15, 30);
+	s.surfaceArea();
+	return 0;
+}
