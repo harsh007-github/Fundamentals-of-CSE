f35155a6af270642cb595cf43d941d3c73d6cd66
harsh007-github <khandelwalharsh401@gmail.com>
2023-02-17 22:40:58 +0530

    This pointer added

--- OOPs in C++/Pointer(Object & this)/pointer.cpp
+++ OOPs in C++/Pointer(Object & this)/pointer.cpp
@@ -24,3 +24,29 @@ int main()
 	ptr->showData();
 	return 0;
 }
+
+
+//Program to illustrate this pointer 
+#include<iostream>
+using namespace std;
+class dimensions
+{
+	private:
+		int a , b;
+	public:
+		void setData(int a, int b)
+		{
+			this -> a = a;
+			this -> b = b;
+		}
+		void showData()
+		{
+			cout << a << " " << b;
+		}
+};
+int main()
+{
+	dimensions d;
+	d.setData(15, 15);
+	d.showData();	
+}
