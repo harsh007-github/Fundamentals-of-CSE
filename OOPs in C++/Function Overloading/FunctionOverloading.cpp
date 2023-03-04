411f1e04bed1acbef455e66732ac0daa749605bd
harsh007-github <khandelwalharsh401@gmail.com>
2023-03-04 21:22:21 +0530

    Virtual Functions added

--- OOPs in C++/Function Overloading/FunctionOverloading.cpp
+++ OOPs in C++/Function Overloading/FunctionOverloading.cpp
@@ -31,3 +31,31 @@ int main ()
 	b.print(15.236, 25.2365);
 	return 0;
 }
+
+//Program to illustrate Virtual Function
+#include<iostream>
+using namespace std;
+class base 
+{
+	public:
+		virtual void printContent()
+		{
+			cout << "This is some base class content";
+		}
+};
+class derived : public base
+{
+	public:
+		void printContent()
+		{
+			cout << "This is some derived class content";	
+		}	
+};
+int main()
+{
+	base* bptr;
+	derived der;
+	bptr = &der;
+	bptr -> printContent();
+	return 0;
+}
