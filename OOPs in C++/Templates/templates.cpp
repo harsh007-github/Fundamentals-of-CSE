e1b02b894bba06cf0aa25ecf57cda65e0d5eebff
harsh007-github <khandelwalharsh401@gmail.com>
2023-03-26 21:45:54 +0530

    Class Templates added

--- OOPs in C++/Templates/templates.cpp
+++ OOPs in C++/Templates/templates.cpp
@@ -1,4 +1,4 @@
-//Program to illustrate Templates
+//Program to illustrate Function Templates
 #include<iostream>
 using namespace std;
 
@@ -12,6 +12,35 @@ int main()
 {
 	int a = 15, b = 16;
 	cout << maxInt(a , b) << " ";
+}
+//Program to illustrate class Templates
+#include<iostream>
+using namespace std;
+template<class x>
+class area
+{
+	private:
+	x num1 , num2;	
+	public:
+		void setData(x a , x b)
+		{
+			num1 = a;
+			num2 = b;
+		}
+		void printData()
+		{
+			cout<<"Area is " << num1 * num2; 
+		}
+};
+int main()
+{
+	area <int>a;
+	int c , d;
+	cout << "Enter dimensions of rectangle:";
+	cin >> c >> d;
+	a.setData(c , d);
+	a.printData();
+	return 0;
 }
 
 
