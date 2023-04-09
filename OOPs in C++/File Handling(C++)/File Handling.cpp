bb2011feae322bdaa7a2636b258416a811e51484
harsh007-github <khandelwalharsh401@gmail.com>
2023-04-09 22:12:51 +0530

    use of ifstream added

--- OOPs in C++/File Handling(C++)/File Handling.cpp
+++ OOPs in C++/File Handling(C++)/File Handling.cpp
@@ -1,4 +1,4 @@
-//Basic Program to illustrate file handling
+// Basic Program to illustrate file handling
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -8,4 +8,28 @@ int main()
 	ofstream fout;
 	fout.open("names.txt");
 	fout << "hello";
+}
+
+// Insert data in file and read it
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	ofstream fout;
+	fout.open("touch.txt");
+	fout << "Hello there this text will be stored inside the file";
+	fout.close();
+
+	ifstream fin;
+	fin.open("touch.txt");
+
+	// Read the contents of the file into a string variable
+	string contents;
+	getline(fin, contents);
+
+	// Print the contents to the console
+	cout << contents << endl;
+
+	fin.close();
 }
