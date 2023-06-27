90d41c6a1332273674f857368275bc9d20d27ea4
harsh007-github <khandelwalharsh401@gmail.com>
2023-06-27 15:12:45 +0530

    Functions in JS

---
+++ JavaScript/Functions/script.js
@@ -0,0 +1,18 @@
+//Functions in JavaScript
+var a = 15;
+
+first();
+
+second();
+
+console.log(a);
+
+function first() {
+  var a = "Harsh";
+  console.log(a);
+}
+
+function second() {
+  var a = 987;
+  console.log(a);
+}
